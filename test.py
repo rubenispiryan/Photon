6f56cf4c4ea7e481fd33477d48651a9c3e4187f5
rubenispiryan <ruben.ispir@gmail.com>
2024-10-06 12:27:18 -0400

    Add Test Snapshot Generation

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import sys
 
 
 def execute(subcommand, filename, flag=''):
@@ -22,10 +23,33 @@ def read_expected(filename):
         expected = f.read()
         return expected
 
+def create_expected(filename, expected):
+    with open(filename, 'wb') as f:
+        f.write(expected)
 
-def main():
+
+def create_examples():
     filenames = filter(lambda x: x.endswith('.phtn'), get_files('./examples'))
     for filename in filenames:
+        simulated_out = execute('sim', filename)
+        compiled_out = execute('com', filename, flag='--run')
+        assert compiled_out == simulated_out, (
+            f'Output from compilation:\n'
+            f'  {compiled_out}\n'
+            f'Output from simulation:\n'
+            f'  {simulated_out}\n'
+            f'Compilation and Simulation of {filename} do not match during snapshot')
+        create_expected(filename[:-4] + 'test', compiled_out)
+        print(f'Snapshot of {filename} created successfully')
+
+
+def check_examples():
+    filenames = filter(lambda x: x.endswith('.phtn'), get_files('./examples'))
+    for filename in filenames:
+        expected_filename = filename[:-4] + 'test'
+        if not os.path.exists(expected_filename):
+            print(f'Tests for {filename} do not exist')
+            continue
         simulated_out = execute('sim', filename)
         compiled_out = execute('com', filename, flag='--run')
         expected = read_expected(filename[:-4] + 'test')
@@ -45,4 +69,7 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) == 1:
+        check_examples()
+    elif len(sys.argv) == 2 and sys.argv[1] == '--snapshot':
+        create_examples()
