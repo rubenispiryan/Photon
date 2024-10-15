ed2c8d79634799ee160f4c1ea50705e7ac3961d1
rubenispiryan <ruben.ispir@gmail.com>
2024-10-15 11:27:09 -0400

    Show Compiler Output During Error In Execution

--- test.py
+++ test.py
@@ -5,7 +5,12 @@ from typing import List
 
 
 def execute(subcommand: str, filename: str, flag: str ='') -> bytes:
-    return subprocess.check_output(f'python3 photon.py {subcommand} {filename} {flag}', shell=True)
+    try:
+        return subprocess.check_output(f'python3 photon.py {subcommand} {filename} {flag}', shell=True)
+    except subprocess.CalledProcessError as e:
+        print(f'Command: "python3 photon.py {subcommand} {filename} {flag}" failed with error:')
+        print(e.output)
+        exit(1)
 
 
 def get_files(folder: str) -> List[str]:
